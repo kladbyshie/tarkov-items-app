2c2aa7115a5a634792b69619df89788e72f0c74a
ngaragulya <ngaragulya@gmail.com>
2020-04-27 17:17:27 -0400

    updated scraper

--- newscraper.py
+++ newscraper.py
@@ -3,10 +3,18 @@ import time
 from datetime import date
 import pandas as pd
 from bs4 import BeautifulSoup
+import os
 
 #This version is MUCH faster as it uses Selenium only to infinite scroll, and uses BS4 to parse
 def marketscrape():
-    driver = webdriver.Chrome()
+    #these options were added so that Heroku could run selenium
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     url  = 'https://tarkov-market.com/'
     driver.get(url)
 
